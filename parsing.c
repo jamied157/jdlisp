96fbcdc3c79ac12cedb0676b2f96c0e23f7b2dff
Jamie Dougherty <jamied157@gmail.com>
2020-05-03 15:14:17 +0100

    Printing and Error builtins

--- parsing.c
+++ parsing.c
@@ -1273,6 +1273,8 @@ void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
 }
 
 lval* builtin_load(lenv* e, lval* v);
+lval* builtin_error(lenv* e, lval* v);
+lval* builtin_print(lenv* e, lval* v);
 
 void lenv_add_builtins(lenv* e) {
 	/* List Functions */
@@ -1288,6 +1290,8 @@ void lenv_add_builtins(lenv* e) {
 	lenv_add_builtin(e, "exit", builtin_exit);
 	lenv_add_builtin(e, "\\", builtin_lambda);
 	lenv_add_builtin(e, "load", builtin_load);
+	lenv_add_builtin(e, "error", builtin_error);
+	lenv_add_builtin(e, "print", builtin_print);
 
 	/* Mathematical Funtions */
 	lenv_add_builtin(e, "+", builtin_add);
@@ -1416,6 +1420,31 @@ lval* builtin_load(lenv* e, lval* a) {
 		/* Cleanup and return error */
 		return err;
 	}
+}
+
+lval* builtin_print(lenv* e, lval* a) {
+
+	/* Print each argument followed by a space */
+	for (int i = 0; i < a->count; i++) {
+		lval_print(a->cell[i]); putchar(' ');
+	}
+
+	/* Print a newline and delete arguments */
+	putchar('\n');
+	lval_del(a);
+	return lval_sexpr();
+}
+
+lval* builtin_error(lenv* e, lval* a) {
+	CHECK_ARG_NUM(a, 1, "error")
+	TYPE_CHECK(a, 0, LVAL_STR, "error")
+
+	/* Construct Error from first argument */
+	lval* err = lval_err(a->cell[0]->str);
+
+	/* Delete arguments and return */
+	lval_del(a);
+	return err;
 }
 
 int main(int argc, char** argv){
